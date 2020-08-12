b734001cf0e17b10bfce7965cfd168afac8dd650
Oscar G <mgraylugo@gmail.com>
2020-08-12 17:12:15 -0500

    Update Buscador.php
    Modifica

--- Buscador.php
+++ Buscador.php
@@ -2,54 +2,62 @@
 
 <?php
 // Mostrar datos Seleccionados
-  //echo("ENCABEZADO estoy en Buscador.php");
-  //echo nl2br("\n");  echo nl2br("\n");
-  //$("#idCiudad").change(function(event){
-  //  $selCd=document.getElementById("idCiudad").value;
-  //  print("Ciudad seleccionada:".$selcd);
- //echo rtrim($str,"World!");
-  //})
   $buscaCiudad="New York";
   $buscaTipo="Casa";
   $buscaCiudad=$_POST['cd'];
-  $rnPrecio=$_POST['rPrec'];
-  //$cam=SELECT CONVERT(varchar,$rPrecio);
-  echo " Busca la Ciudad de : ".$buscaCiudad.PHP_EOL;
-  echo " Busca entre el precio : ".$rnPrecio.PHP_EOL;
-  echo " Busca entre el precio CHAR : ".$cam.PHP_EOL;
-//  echo " Busca entre el precio bajo : ".rtrim($rnPrecio,";").PHP_EOL;
-//  echo " Busca entre el precio alto : ".ltrim($rnPrecio,";").PHP_EOL;
-  echo PHP_EOL;
+  $buscaTipo=$_POST['tp'];
+  $rnPrecioInicial=$_POST['rPrecI'];
+  $rnPrecioFinal=$_POST['rPrecF'];
+ //echo "<script>window.open='index.html';</script>";
+  //echo " en PHP Busca la Ciudad de : ".$buscaCiudad." Tipo: ".$buscaTipo.PHP_EOL;
+  //echo " Busca entre el precio : ".$rnPrecioInicial." y ".$rnPrecioFinal.PHP_EOL;
+  //echo PHP_EOL;
+
 /*******************************************/
 // el archivo tiene que ir como lectura "r"
   $archi=fopen("./data-1.json","r");
   $lee_dat=fread($archi,filesize("./data-1.json"));
   $dato=json_decode($lee_dat,true);
-  $i=-1;
+  $i=0;
   foreach ($dato as $key => $seleccion) {
-    $i=$i+1;
-    if($buscaCiudad=="Todo"){
-      if($buscaTipo=="Todo"){
-        echo "Error en selección: NO se perimte seleccionar Todo en Ciudad y Tipo";
-      }
-      else{
-        if($seleccion['Tipo']==$buscaTipo){
-          echo json_encode($seleccion);echo nl2br("\n");echo nl2br("\n");
+
+    $pre2=$seleccion['Precio'];
+    $pre1=str_replace("$","",$pre2);
+    $pre=str_replace(",","",$pre1);
+    if($rnPrecioInicial<=$pre && $pre<=$rnPrecioFinal)
+    {
+      if($buscaCiudad=="Todo"){
+        if($buscaTipo=="Todo"){
+          $i=1;
+          echo "Error en selección: NO se perimte seleccionar Todo en Ciudad y Tipo";
+        }
+        else{
+          if($seleccion['Tipo']==$buscaTipo){
+            $i=1;
+            echo json_encode($seleccion);echo PHP_EOL;echo PHP_EOL;
+          }
         }
       }
-    }
-    else {
-        if($seleccion['Ciudad']==$buscaCiudad){
-          if($seleccion['Tipo']=="Todo"){
-            echo json_encode($seleccion);echo nl2br("\n");echo nl2br("\n");
-          }
-          else{
-            if($seleccion['Tipo']==$buscaTipo){
-              echo json_encode($seleccion);echo nl2br("\n");echo nl2br("\n");
+      else {
+          if($seleccion['Ciudad']==$buscaCiudad){
+            if($buscaTipo=="Todo"){
+              $i=1;
+              echo json_encode($seleccion);echo PHP_EOL;echo PHP_EOL;
+            }
+            else{
+              if($seleccion['Tipo']==$buscaTipo){
+                $i=1;
+                echo json_encode($seleccion);echo PHP_EOL;echo PHP_EOL;
+              }
             }
           }
-        }
+      }
     }
 
+    $pre1="";$pre="";
+  }
+
+  if($i==0){
+    echo "No se encontró dato con las características solicitadas";
   }
 ?>
