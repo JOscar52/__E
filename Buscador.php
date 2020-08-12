43fde6c5b2e1501018f1d364dcd77b6c813446bb
Oscar G <mgraylugo@gmail.com>
2020-08-12 17:08:00 -0500

    Update Buscador.php
    Agrega y Modifica

--- Buscador.php
+++ Buscador.php
@@ -1,76 +1,55 @@
 
 
 <?php
-// Mostrar datos
-  // archivo openFile-w.php
-  echo "Lee un archivo ";
-
-  $campo="username";
-
-  //$titulo = $_POST['ajaxvarjs3'];
-  //echo json_encode($titulo);
-  //$tex = $_POST['ajax4'];
-
-  //$nuevfile= fopen("./".$titulo,"a") or die ("Error en la creacion del archivo");
+// Mostrar datos Seleccionados
+  //echo("ENCABEZADO estoy en Buscador.php");
+  //echo nl2br("\n");  echo nl2br("\n");
+  //$("#idCiudad").change(function(event){
+  //  $selCd=document.getElementById("idCiudad").value;
+  //  print("Ciudad seleccionada:".$selcd);
+ //echo rtrim($str,"World!");
+  //})
+  $buscaCiudad="New York";
+  $buscaTipo="Casa";
+  $buscaCiudad=$_POST['cd'];
+  $rnPrecio=$_POST['rPrec'];
+  //$cam=SELECT CONVERT(varchar,$rPrecio);
+  echo " Busca la Ciudad de : ".$buscaCiudad.PHP_EOL;
+  echo " Busca entre el precio : ".$rnPrecio.PHP_EOL;
+  echo " Busca entre el precio CHAR : ".$cam.PHP_EOL;
+//  echo " Busca entre el precio bajo : ".rtrim($rnPrecio,";").PHP_EOL;
+//  echo " Busca entre el precio alto : ".ltrim($rnPrecio,";").PHP_EOL;
+  echo PHP_EOL;
 /*******************************************/
 // el archivo tiene que ir como lectura "r"
   $archi=fopen("./data-1.json","r");
   $lee_dat=fread($archi,filesize("./data-1.json"));
   $dato=json_decode($lee_dat,true);
-  //$busca="456";
-  //$cambio="cambio_username  sss";
-  foreach ($dato as $key => $dato_campo) {
-    /*
-    if ($busca==$dato_campo['password']){
-      $dat_cam=$dato_campo["username"];
-      echo json_encode("eeeeel dato es campo ".$campo."\n");
-      $respon["mens"]="mensaje el dato es ".$dat_cam."\n";
-      echo json_encode($respon);
-
-
+  $i=-1;
+  foreach ($dato as $key => $seleccion) {
+    $i=$i+1;
+    if($buscaCiudad=="Todo"){
+      if($buscaTipo=="Todo"){
+        echo "Error en selección: NO se perimte seleccionar Todo en Ciudad y Tipo";
+      }
+      else{
+        if($seleccion['Tipo']==$buscaTipo){
+          echo json_encode($seleccion);echo nl2br("\n");echo nl2br("\n");
+        }
+      }
     }
-    */
-    //en jquery archivo json:  $(".pt2").html(pbt);
-    echo json_encode($dato_campo);
-  }
-/*******************************************/
-/*    // Actualiza
-    session_start();
-
-    $busca="456";
-    $cambio="cambio_username NUEVO";
-
-    $_SESSION['password']=$busca;
-    updateData($_SESSION["password"],'username',$cambio);
-
-    function updateData($busca, $archi, $cambio){
-
-      $archi=fopen("./json.json","r");
-      $lee_dat=fread($archi,filesize("./json.json"));
-      $dato=json_decode($lee_dat,true);
-
-      $data = $dato;
-      if (isset($data)) {
-        $i=-1;
-        foreach ($data as $key => $value) {
-          $i=$i+1;
-          if ($value['password']==$busca) {
-              $data[$i]['username']=$cambio;
-            echo json_encode("estoy en update antes de write i=".$i." busca ".$busca."  ".$data[0]['username']);
+    else {
+        if($seleccion['Ciudad']==$buscaCiudad){
+          if($seleccion['Tipo']=="Todo"){
+            echo json_encode($seleccion);echo nl2br("\n");echo nl2br("\n");
+          }
+          else{
+            if($seleccion['Tipo']==$buscaTipo){
+              echo json_encode($seleccion);echo nl2br("\n");echo nl2br("\n");
+            }
           }
         }
-        echo json_encode("estoy en update antes de write ".$busca." ".$archi."  ".$cambio);
-        writeDataFile($data);
-      }
     }
 
-    function writeDataFile($data_array){
-      if (sizeof($data_array)>0) {
-        $data_file = fopen("./json.json","w");
-        fwrite($data_file, json_encode($data_array));
-        fclose($data_file);
-      }
-
-    }
-    */
+  }
 ?>
