13ddf3833bb5f8e91c49af1ab49e19d07aab8cc4
Oscar G <mgraylugo@gmail.com>
2020-08-13 04:09:07 -0500

    Update indexSelecciona.js
    Cambios

--- js/indexSelecciona.js
+++ js/indexSelecciona.js
@@ -1,51 +1,98 @@
-/*
-  Creación de una función para datos de selección de Ciudad y Tipo
-*/
 
-//alert("XRA estoy en indexSelecciona.js");
-//$("#idCiudad").change(function(event){
-$("#formulario").submit(function(event){  // detecta botón
-  //alert("estoy en submit");
-var selCd=document.getElementById("idCiudad").value;
-var selTp=document.getElementById("idTipo").value;
-var rPrecio=document.getElementById("rangoPrecio").value;
-//var PrecioInicial=rtrim(rPrecio,";");
-//busca = patron.exec("3c491a-9d1d6-91br");
-var pospc=rPrecio.indexOf(";");
-var PrecioInicial=rPrecio.slice(0,pospc);
-var PrecioFinal=rPrecio.slice(pospc+1);
-//  alert("Ciudad seleccionada: "+selCd+" tipo: "+selTp);
-//  alert("Precio : "+rPrecio+" inicial "+PrecioInicial+" Final "+PrecioFinal);
+// indexSelecciona.js
 
+$(document).ready(function () {
+    $('#formulario').submit(function (e) {
+        e.preventDefault();
 
-//});
-
-//$('#submitButton').submit(function(event){  // detecta botón
-//  alert("Mostrar Selección");
-   //var subvarjs3 = "mostrarTodos";
-   //var subvarjs4 = $('form').find('input[name="hinputname4"]').val();
-   //alert(" en js "+subvarjs3);alert(subvarjs4);
-   //event.preventDefault();
-   $.ajax(
-   {
-//     url: './Bus.php',
-     url: './Buscador.php',
-// agrego 2 l�neas
-//    cache: false,
-//    contentType: false,
-//    processData: false,
-// agregu� 2 l�neas
-    // dataType:"json",
-     type: 'POST',
-     data: {cd: selCd,
-            tp: selTp,
-            rPrecI: PrecioInicial,
-            rPrecF: PrecioFinal
-           }
-            //rPrecI:rPrecioInicial}
-
-   }
-  ).done(function(data){
-      alert(data);
-  });
+        $.ajax({
+            type: "POST",
+            url: './buscador.php',
+            data: $(this).serialize(),
+            success: function (response) {
+                var jsonData = JSON.parse(response);
+                $(".informacion").empty();
+                if (jsonData) {
+                    colocaHtml(jsonData);
+                }
+            }
+        });
+    });
 });
+
+$.ajax({
+    url: "./data-1.json",
+    type: "GET",
+    data: {},
+    dataType: 'json',
+    success: function (data) {
+        var ciudad = {};
+        var ciudades = data.filter(function (e) {
+            if (ciudad[e.Ciudad]) {
+                return false;
+            } else {
+                ciudad[e.Ciudad] = true
+                return true;
+            }
+
+        });
+        for (var i = 0; i <= 5; i++) {
+            $("#selectCiudad").append("<option value= '" + ciudades[i].Ciudad + "'>" + ciudades[i].Ciudad + "</option>");
+        }
+
+        var tipo = {};
+        var tipos = data.filter(function (e) {
+            if (tipo[e.Tipo]) {
+                return false;
+            } else {
+                tipo[e.Tipo] = true
+                return true;
+            }
+        });
+        for (var i = 0; i <= 2; i++) {
+            $("#selectTipo").append("<option value='" + tipos[i].Tipo + "'>" + tipos[i].Tipo + "</option>");
+        }
+
+        $('select').material_select();
+
+    }
+})
+
+$('#mostrarTodos').click(function () {
+    mostrarTodos();
+});
+
+function mostrarTodos() {
+    $.ajax({
+        url: "mostrar.php",
+        type: "GET",
+        success: function (response) {
+            response = JSON.parse(response);
+            $(".informacion").empty();
+            colocaHtml(response);
+        }
+    });
+}
+//  <div class="card-action"> <a href="#">Ver mas</a> </div>
+function colocaHtml(arreglo) {
+    $.each(arreglo, function (i, data) {
+        $(".informacion").append(`
+            <div class="tituloContenido card itemMostrado">
+                <img src="img/home.jpg">
+                <div class="card-stacked">
+                   <div class="card-content">
+                      <b>Id:</b> ${data.Id}<br>
+                      <b>Dirección:</b> ${data.Direccion} <br>
+                      <b>Ciudad:</b> ${data.Ciudad}<br>
+                      <b>Telefono:</b> ${data.Telefono}<br>
+                      <b>Codigo Postal:</b> ${data.Codigo_Postal}<br>
+                      <b>Tipo:</b> ${data.Tipo}<br>
+                      <b>Precio:</b> ${data.Precio}<br>
+                      </div>
+
+                  </div> </div>
+                  </div>
+
+        `)
+    });
+}
