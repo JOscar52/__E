78b8c38f3f02baf45872d75cd67d045d6356174c
Oscar G <mgraylugo@gmail.com>
2020-08-13 04:09:15 -0500

    Create mostrar.php
    Cambios

---
+++ mostrar.php
@@ -0,0 +1,25 @@
+
+
+<?php
+// mostrar.php
+$data = file_get_contents("data-1.json");
+$jsonData = json_decode($data);
+
+
+foreach ($jsonData as $key => $obj) {
+    $Id = $obj->Id;
+    $Direccion = $obj->Direccion;
+    $Ciudad = $obj->Ciudad;
+    $Telefono = $obj->Telefono;
+    $Codigo_Postal = $obj->Codigo_Postal;
+    $Tipo = $obj->Tipo;
+    $Precio =$obj->Precio;
+
+ array_push($jsonData, $obj);
+
+}
+
+echo json_encode($jsonData);
+
+
+?>
